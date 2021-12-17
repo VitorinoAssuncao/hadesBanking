5ac8c2983927837678faaf3bcf13ae893b757be1
Vitorino Assuncao <vitorino.muller@gmail.com>
2021-12-17 15:05:02 -0300

    feature: testes de validação da criação de conta feitos

--- app/application/validations/create_account_test.go
+++ app/application/validations/create_account_test.go
@@ -2,6 +2,86 @@ package validations
 
 import "testing"
 
+func Test_nameIsNotEmpty(t *testing.T) {
+	t.Run("Nome não etá vazio", func(t *testing.T) {
+		name := "Joao"
+		result := nameIsNotEmpty(name)
+
+		if !result {
+			t.Errorf("Nome não pode ser vazio")
+		}
+	})
+
+	t.Run("Nome está vazio", func(t *testing.T) {
+		name := ""
+		result := nameIsNotEmpty(name)
+
+		if result {
+			t.Errorf("Nome não pode ser vazio")
+		}
+	})
+}
+
+func Test_cpfIsNotEmpty(t *testing.T) {
+	t.Run("CPF não é vazia", func(t *testing.T) {
+		cpf := "123456789"
+		result := cpfIsNotEmpty(cpf)
+
+		if !result {
+			t.Errorf("CPF não pode ser vazio")
+		}
+	})
+
+	t.Run("CPF está vazia", func(t *testing.T) {
+		cpf := ""
+		result := cpfIsNotEmpty(cpf)
+
+		if result {
+			t.Errorf("CPF não poderá ser vazia")
+		}
+	})
+}
+
+func Test_cpfIsJustNumber(t *testing.T) {
+	t.Run("CPF é apenas números", func(t *testing.T) {
+		cpf := "12345678912"
+		result := cpfIsJustNumbers(cpf)
+
+		if !result {
+			t.Errorf("CPF deverá conter apenas números")
+		}
+	})
+
+	t.Run("CPF contem simbolos", func(t *testing.T) {
+		cpf := "123.456.789-12"
+		result := cpfIsJustNumbers(cpf)
+
+		if result {
+			t.Errorf("CPF deverá conter apenas números")
+		}
+	})
+}
+
+func Test_secretIsNotEmpty(t *testing.T) {
+	t.Run("Senha não é vazia", func(t *testing.T) {
+		secret := "12345"
+		result := secretIsNotEmpty(secret)
+
+		if !result {
+			t.Errorf("Senha não pode ser vazia")
+		}
+	})
+
+	t.Run("Senha está vazia", func(t *testing.T) {
+		secret := ""
+		result := secretIsNotEmpty(secret)
+
+		if result {
+			t.Errorf("Senha não poderá ser vazia")
+		}
+	})
+}
+
 func Test_balanceIsPositive(t *testing.T) {
 	t.Run("Realizando o teste com um valor de Saldo positivo", func(t *testing.T) {
 		balance := 10
