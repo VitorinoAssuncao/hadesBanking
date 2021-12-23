96a3bd20611551bfc475ff9552fab29b5aaf2885
Vitorino Assuncao <vitorino.muller@gmail.com>
2021-12-23 15:37:05 -0300

    refactor: ajuste nos campos UUID e ID (mudados para texto e serial respectivamente)

--- app/gateway/database/postgres/migrations/000002_create_transfers_table.up.sql
+++ app/gateway/database/postgres/migrations/000002_create_transfers_table.up.sql
@@ -2,10 +2,10 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS public.transfers
 (
-    id                  integer     NOT NULL,
-    external_id         uuid        NOT NULL,
-    account_origin_id   uuid        NOT NULL,
-    account_destiny_id  uuid        NOT NULL,
+    id                  SERIAL     NOT NULL,
+    external_id         VARCHAR(36)        NOT NULL,
+    account_origin_id   VARCHAR(36)        NOT NULL,
+    account_destiny_id  VARCHAR(36)        NOT NULL,
     ammount             bigint      NOT NULL DEFAULT 0,
     created_at          timestamp with time zone    NOT NULL,
     
