699960bb824a230bd5499ffd749bad19cbcfc18c
Vitorino Assuncao <vitorino.muller@gmail.com>
2021-12-23 09:43:26 -0300

    test: adicionados testes de erro

--- app/gateway/database/postgres/accounts/account_test.go
+++ app/gateway/database/postgres/accounts/account_test.go
@@ -35,11 +35,33 @@ func Test_Create(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{name: "cadastro duplicado",
+			input: account.Account{
+				ID:         "d3280f8c-570a-450d-89f7-3509bc84980d",
+				Name:       "Joao da Silva",
+				Cpf:        "38330499912",
+				Balance:    10000,
+				Created_at: time.Now(),
+			},
+			want: account.Account{
+				ID:      "",
+				Name:    "",
+				Cpf:     "",
+				Balance: 0,
+			},
+			wantErr: true,
+		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := accountRepository.Create(ctx, test.input)
+
+			if test.name == "cadastro duplicado" {
+				_, err = accountRepository.Create(ctx, test.input)
+				got = account.Account{}
+			}
+
 			if err == nil {
 				test.want.Created_at = got.Created_at
 			}
@@ -107,18 +129,33 @@ func Test_GetByID(t *testing.T) {
 				Balance: 10000,
 			},
 			wantErr: false,
+		}, {name: "tentar localizar conta que não existe",
+			input: account.Account{
+				ID:         "d3280f8c-570a-450d-89f7-3509bc84980d",
+				Name:       "Joao da Silva",
+				Cpf:        "38330499912",
+				Balance:    10000,
+				Created_at: time.Now(),
+			},
+			want: account.Account{
+				ID:      "d3280f8c-570a-450d-89f7-3509bc849899",
+				Name:    "",
+				Cpf:     "",
+				Balance: 0,
+			},
+			wantErr: true,
 		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			_, err := accountRepository.Create(ctx, test.input)
-			got, err := accountRepository.GetByID(ctx, test.input.ID)
+			got, err := accountRepository.GetByID(ctx, test.want.ID)
 			if err == nil {
 				test.want.Created_at = got.Created_at
 			}
 			assert.Equal(t, (err != nil), test.wantErr)
-			assert.Equal(t, test.want, got)
+			assert.Equal(t, test.want.Name, got.Name)
 		})
 	}
 }
@@ -149,17 +186,33 @@ func Test_GetByCPF(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{name: "tentar localizar a conta com cpf inexistente",
+			input: account.Account{
+				ID:         "d3280f8c-570a-450d-89f7-3509bc84980d",
+				Name:       "Joao da Silva",
+				Cpf:        "38330499912",
+				Balance:    10000,
+				Created_at: time.Now(),
+			},
+			want: account.Account{
+				ID:      "",
+				Name:    "",
+				Cpf:     "38330499999",
+				Balance: 0,
+			},
+			wantErr: true,
+		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			_, err := accountRepository.Create(ctx, test.input)
-			got, err := accountRepository.GetByCPF(ctx, test.input.Cpf)
+			got, err := accountRepository.GetByCPF(ctx, test.want.Cpf)
 			if err == nil {
 				test.want.Created_at = got.Created_at
 			}
 			assert.Equal(t, (err != nil), test.wantErr)
-			assert.Equal(t, test.want, got)
+			assert.Equal(t, test.want.Name, got.Name)
 		})
 	}
 }
