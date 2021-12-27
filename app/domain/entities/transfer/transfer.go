f14d3f6aa8a1b075d7683ced8af064fb3df9ab3c
Vitorino Assuncao <vitorino.muller@gmail.com>
2021-12-27 11:18:35 -0300

    refactor: removidos comentários redundantes

--- app/domain/entities/transfer/transfer.go
+++ app/domain/entities/transfer/transfer.go
@@ -6,20 +6,10 @@ import (
 )
 
 type Transfer struct {
-	//ID único da transfêrencia (Númerico gerado pelo banco)
-	ID int
-
-	External_ID types.TransferID
-
-	//Código da conta de origem
-	Account_origin_id types.AccountOriginID
-
-	//Código da conta de destino
+	ID                     int
+	External_ID            types.TransferID
+	Account_origin_id      types.AccountOriginID
 	Account_destination_id types.AccountDestinyID
-
-	//Valor referente a transfêrencia, em centavos brasileiros (R$)
-	Amount types.Money
-
-	//Data da transfêrencia
-	Created_at time.Time
+	Amount                 types.Money
+	Created_at             time.Time
 }
