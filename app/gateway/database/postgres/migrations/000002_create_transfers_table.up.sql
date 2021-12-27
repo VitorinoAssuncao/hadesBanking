79de8517af71ad2d4570155c0bd506d597f830b0
Vitorino Assuncao <vitorino.muller@gmail.com>
2021-12-27 11:51:56 -0300

    refactor: alterado de serial para identity

--- app/gateway/database/postgres/migrations/000002_create_transfers_table.up.sql
+++ app/gateway/database/postgres/migrations/000002_create_transfers_table.up.sql
@@ -2,14 +2,12 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS public.transfers
 (
-    id                  SERIAL     NOT NULL,
+    id                  INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     external_id         VARCHAR(36)        NOT NULL,
     account_origin_id   VARCHAR(36)        NOT NULL,
     account_destiny_id  VARCHAR(36)        NOT NULL,
     ammount             bigint      NOT NULL DEFAULT 0,
-    created_at          timestamp with time zone    NOT NULL,
-    
-    CONSTRAINT transfers_pkey PRIMARY KEY (id)
+    created_at          timestamp with time zone    NOT NULL
 );
 
 COMMIT;
