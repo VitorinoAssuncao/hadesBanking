1d975ad7901a9da015e66fda404fe86fcbc59f8d
Vitorino Assuncao <vitorino.muller@gmail.com>
2021-12-29 14:08:42 -0300

    refactor: ajustado id transferencias
    Ajustado para ID's de transferencias sejam internos, e criada chave estrangeira (foreign key)

--- app/gateway/database/postgres/migrations/000002_create_transfers_table.up.sql
+++ app/gateway/database/postgres/migrations/000002_create_transfers_table.up.sql
@@ -4,10 +4,13 @@ CREATE TABLE IF NOT EXISTS public.transfers
 (
     id                  INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     external_id         uuid        NOT NULL default gen_random_uuid(),
-    account_origin_id   uuid        NOT NULL,
-    account_destiny_id  uuid        NOT NULL,
+    account_origin_id   INTEGER        NOT NULL,
+    account_destiny_id  INTEGER        NOT NULL,
     amount              bigint      NOT NULL DEFAULT 0,
-    created_at          timestamp with time zone                     NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at          timestamp with time zone                     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_origin_id FOREIGN KEY(account_origin_id) REFERENCES accounts(id),
+    CONSTRAINT fk_destiny_id FOREIGN KEY(account_destiny_id) REFERENCES accounts(id)
 ); 
 
 COMMIT;
