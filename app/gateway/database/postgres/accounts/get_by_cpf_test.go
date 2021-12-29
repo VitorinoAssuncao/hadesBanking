d08f966b8039860841567eaa4d5dd1f50e6112eb
Vitorino Assuncao <vitorino.muller@gmail.com>
2021-12-29 10:44:25 -0300

    refactor: melhoria nos testes

--- app/gateway/database/postgres/accounts/get_by_cpf_test.go
+++ app/gateway/database/postgres/accounts/get_by_cpf_test.go
@@ -13,18 +13,27 @@ func Test_GetByCPF(t *testing.T) {
 	database := databaseTest
 	accountRepository := NewAccountRepository(database)
 	testCases := []struct {
-		name    string
-		input   account.Account
-		want    account.Account
-		wanted  string
-		wantErr bool
+		name      string
+		input     string
+		runBefore func() (value string)
+		want      account.Account
+		wantErr   bool
 	}{
 		{
 			name: "localizada a conta utilizando-se do cpf, e retorna os dados da mesma",
-			input: account.Account{
-				Name:    "Joao da Silva",
-				CPF:     "38330499912",
-				Balance: 10000,
+			runBefore: func() (value string) {
+				input := account.Account{
+					Name:    "Joao da Silva",
+					CPF:     "38330499912",
+					Balance: 10000,
+				}
+				created, err := accountRepository.Create(ctx, input)
+
+				if err == nil {
+					value = created.CPF
+				}
+
+				return value
 			},
 			want: account.Account{
 				ExternalID: "d3280f8c-570a-450d-89f7-3509bc84980d",
@@ -32,18 +41,12 @@ func Test_GetByCPF(t *testing.T) {
 				CPF:        "38330499912",
 				Balance:    10000,
 			},
-			wanted:  "38330499912",
 			wantErr: false,
 		},
 		{
-			name: "retorna erro ao tentar localizar conta com cpf inexistente",
-			input: account.Account{
-				Name:    "Joao da Silva",
-				CPF:     "38330499912",
-				Balance: 10000,
-			},
+			name:    "retorna erro ao tentar localizar conta com cpf inexistente",
+			input:   "38330499999",
 			want:    account.Account{},
-			wanted:  "38330499999",
 			wantErr: true,
 		},
 	}
@@ -51,8 +54,12 @@ func Test_GetByCPF(t *testing.T) {
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			TruncateTable(database)
-			_, err := accountRepository.Create(ctx, test.input)
-			got, err := accountRepository.GetByCPF(ctx, test.wanted)
+			if test.runBefore != nil {
+				test.input = test.runBefore()
+			}
+
+			got, err := accountRepository.GetByCPF(ctx, test.input)
+
 			if err == nil {
 				test.want.CreatedAt = got.CreatedAt
 				test.want.ExternalID = got.ExternalID
--- app/gateway/database/postgres/accounts/get_by_id_test.go
+++ app/gateway/database/postgres/accounts/get_by_id_test.go
@@ -15,20 +15,25 @@ func Test_GetByID(t *testing.T) {
 	accountRepository := NewAccountRepository(database)
 	testCases := []struct {
 		name      string
-		input     account.Account
 		want      account.Account
-		runBefore func(value types.AccountID) types.AccountID
-		wanted    types.AccountID
+		runBefore func() (value types.AccountID)
+		input     types.AccountID
 		wantErr   bool
 	}{
 		{
 			name: "localizado a conta usando o ID externo (uuid), e retorna os dados da mesma ",
-			input: account.Account{
-				Name:    "Joao da Silva",
-				CPF:     "38330499912",
-				Balance: 10000,
-			},
-			runBefore: func(value types.AccountID) types.AccountID {
+			runBefore: func() (value types.AccountID) {
+				input := account.Account{
+					Name:    "Joao da Silva",
+					CPF:     "38330499912",
+					Balance: 10000,
+				}
+				created, err := accountRepository.Create(ctx, input)
+
+				if err == nil {
+					value = created.ExternalID
+				}
+
 				return value
 			},
 			want: account.Account{
@@ -36,17 +41,12 @@ func Test_GetByID(t *testing.T) {
 				CPF:     "38330499912",
 				Balance: 10000,
 			},
-			wanted:  "d3280f8c-570a-450d-89f7-3509bc84980d",
+			input:   "d3280f8c-570a-450d-89f7-3509bc84980d",
 			wantErr: false,
 		}, {
-			name: "retorna dados vazios e erro, ao tentar localizar conta com ID inexistente",
-			input: account.Account{
-				Name:    "Joao da Silva",
-				CPF:     "38330499912",
-				Balance: 10000,
-			},
+			name:    "retorna dados vazios e erro, ao tentar localizar conta com ID inexistente",
 			want:    account.Account{},
-			wanted:  "d3280f8c-570a-450d-89f7-3509bc849899",
+			input:   "d3280f8c-570a-450d-89f7-3509bc849899",
 			wantErr: true,
 		},
 	}
@@ -54,13 +54,10 @@ func Test_GetByID(t *testing.T) {
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			TruncateTable(database)
-			created, err := accountRepository.Create(ctx, test.input)
-
-			if test.runBefore != nil && err == nil {
-				test.wanted = test.runBefore(created.ExternalID)
+			if test.runBefore != nil {
+				test.input = test.runBefore()
 			}
-
-			got, err := accountRepository.GetByID(ctx, types.AccountID(test.wanted))
+			got, err := accountRepository.GetByID(ctx, types.AccountID(test.input))
 			if err == nil {
 				test.want.CreatedAt = got.CreatedAt
 				test.want.ID = got.ID
