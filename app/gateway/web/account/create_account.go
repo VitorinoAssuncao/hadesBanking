ffa5fdbb430d5c3944e5f2610e6ec840afbff238
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-06 08:20:04 -0300

    style: change in nomenclature

--- app/gateway/web/account/create_account.go
+++ app/gateway/web/account/create_account.go
@@ -19,11 +19,11 @@ func (controller *Controller) Create(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal(reqBody, &accountData)
 
-	account_output, err := controller.usecase.Create(r.Context(), *accountData)
+	accountOutput, err := controller.usecase.Create(r.Context(), *accountData)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	json.NewEncoder(w).Encode(account_output)
+	json.NewEncoder(w).Encode(accountOutput)
 }
