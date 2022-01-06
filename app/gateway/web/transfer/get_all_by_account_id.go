95b27191bb9d8daf84469988f6185a483f9ea7fe
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-06 10:00:42 -0300

    feat: added getAllByAccountID
    Adicionada rota para pegar todas as transferencias dado um ExternalID

---
+++ app/gateway/web/transfer/get_all_by_account_id.go
@@ -0,0 +1,23 @@
+package transfer
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"stoneBanking/app/domain/types"
+
+	"github.com/gorilla/mux"
+)
+
+func (controller Controller) GetAllByAccountID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	accountId := vars["account_id"]
+	transfersOutput, err := controller.usecase.GetAllByAccountID(context.Background(), types.AccountExternalID(accountId))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(transfersOutput)
+}
