7aadfbfb141cdb83982b5aad55a6b7b91d63dae7
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-06 08:55:26 -0300

    refactor: added route create_transfer

--- app/gateway/web/server/handler.go
+++ app/gateway/web/server/handler.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	accounts "stoneBanking/app/gateway/web/account"
+	transfers "stoneBanking/app/gateway/web/transfer"
 
 	"github.com/gorilla/mux"
 )
@@ -14,11 +15,13 @@ type Server struct {
 
 func New(usecase *UseCaseWrapper) *Server {
 	router := mux.NewRouter().StrictSlash(true)
-	controller := accounts.New(usecase.Accounts)
-	router.HandleFunc("/account", controller.Create).Methods("POST")
-	router.HandleFunc("/account/login", controller.LoginUser).Methods("POST")
-	router.HandleFunc("/accounts", controller.GetAll).Methods("GET")
-	router.HandleFunc("/account/{user_id}/balance", controller.GetBalance).Methods("GET")
+	controller_account := accounts.New(usecase.Accounts)
+	controller_transfer := transfers.New(usecase.Transfer)
+	router.HandleFunc("/account", controller_account.Create).Methods("POST")
+	router.HandleFunc("/account/login", controller_account.LoginUser).Methods("POST")
+	router.HandleFunc("/accounts", controller_account.GetAll).Methods("GET")
+	router.HandleFunc("/account/{user_id}/balance", controller_account.GetBalance).Methods("GET")
+	router.HandleFunc("/transfer", controller_transfer.Create).Methods("POST")
 	log.Fatal(http.ListenAndServe(":3000", router))
 	server := Server{Router: *router}
 	return &server
