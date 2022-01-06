63759cdb67de5a0cd8e13dfe3b5f18a3cf1b6526
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-06 08:22:45 -0300

    feat: add create_transfer
    Adicionada estrutura de criação de transferencia

---
+++ app/gateway/web/transfer/create_transfer.go
@@ -0,0 +1,27 @@
+package transfer
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"stoneBanking/app/application/vo/input"
+)
+
+func (controller Controller) Create(w http.ResponseWriter, r *http.Request) {
+	var transferData = input.CreateTransferVO{}
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.Unmarshal(reqBody, &transferData)
+
+	transferOutput, err := controller.usecase.Create(context.Background(), transferData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	json.NewEncoder(w).Encode(transferOutput)
+}
