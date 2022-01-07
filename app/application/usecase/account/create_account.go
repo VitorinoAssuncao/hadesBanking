8c980cd8a18f505dd122ac75a6919bf43376c911
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-07 14:39:30 -0300

    feat: add validation in create account

--- app/application/usecase/account/create_account.go
+++ app/application/usecase/account/create_account.go
@@ -2,21 +2,26 @@ package account
 
 import (
 	"context"
+	validations "stoneBanking/app/application/usecase/account/validations"
 	"stoneBanking/app/domain/entities/account"
+	customError "stoneBanking/app/domain/errors"
 )
 
 func (usecase *usecase) Create(ctx context.Context, accountData account.Account) (account.Account, error) {
-
-	_, err := usecase.accountRepository.GetByCPF(ctx, accountData.CPF)
+	err := validations.ValidateAccountData(accountData)
+	if err != nil {
+		return account.Account{}, err
+	}
+	_, err = usecase.accountRepository.GetByCPF(ctx, accountData.CPF)
 	//validate if account with that cpf exist, if not continue the creation of a new account
 	if err == nil {
-		return account.Account{}, ErrorAccountCPFExists
+		return account.Account{}, customError.ErrorAccountCPFExists
 	}
 
 	accountResult, err := usecase.accountRepository.Create(ctx, accountData)
 
 	if err != nil {
-		return account.Account{}, ErrorCreateAccount
+		return account.Account{}, customError.ErrorCreateAccount
 	}
 
 	return accountResult, nil
