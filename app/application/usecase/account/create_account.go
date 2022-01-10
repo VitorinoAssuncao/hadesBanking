0adf5868f0bcb2443c8bda2f291a359e26f29454
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-10 17:17:36 -0300

    refactor: add validations if err the error is in database

--- app/application/usecase/account/create_account.go
+++ app/application/usecase/account/create_account.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"context"
+	"database/sql"
 	validations "stoneBanking/app/application/usecase/account/validations"
 	"stoneBanking/app/domain/entities/account"
 	customError "stoneBanking/app/domain/errors"
@@ -12,10 +13,15 @@ func (usecase *usecase) Create(ctx context.Context, accountData account.Account)
 	if err != nil {
 		return account.Account{}, err
 	}
+
 	_, err = usecase.accountRepository.GetByCPF(ctx, accountData.CPF.ToString())
 	//validate if account with that cpf exist, if not continue the creation of a new account
 	if err == nil {
 		return account.Account{}, customError.ErrorAccountCPFExists
+	}
+
+	if err != sql.ErrNoRows {
+		return account.Account{}, customError.ErrorCreateAccount
 	}
 
 	accountResult, err := usecase.accountRepository.Create(ctx, accountData)
