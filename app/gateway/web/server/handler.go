7aea0049c95805f6c4d9efb3feb408d0e7795608
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-10 09:05:11 -0300

    refactor: retire variable in the routes

--- app/gateway/web/server/handler.go
+++ app/gateway/web/server/handler.go
@@ -20,9 +20,9 @@ func New(usecase *UseCaseWrapper) *Server {
 	router.HandleFunc("/account", controller_account.Create).Methods("POST")
 	router.HandleFunc("/account/login", controller_account.LoginUser).Methods("POST")
 	router.HandleFunc("/accounts", controller_account.GetAll).Methods("GET")
-	router.HandleFunc("/account/{user_id}/balance", controller_account.GetBalance).Methods("GET")
+	router.HandleFunc("/account/balance", controller_account.GetBalance).Methods("GET")
 	router.HandleFunc("/transfer", controller_transfer.Create).Methods("POST")
-	router.HandleFunc("/transfer/{account_id}", controller_transfer.GetAllByAccountID).Methods("GET")
+	router.HandleFunc("/transfer", controller_transfer.GetAllByAccountID).Methods("GET")
 	log.Fatal(http.ListenAndServe(":3000", router))
 	server := Server{Router: *router}
 	return &server
