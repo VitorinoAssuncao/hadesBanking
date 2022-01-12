7a7ffd55040324325cda942fd2a1c47b329367a1
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-12 13:26:54 -0300

    refactor: changed to validate a specific error

--- app/application/usecase/account/get_balance_test.go
+++ app/application/usecase/account/get_balance_test.go
@@ -3,9 +3,9 @@ package account
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"stoneBanking/app/domain/entities/account"
 	"stoneBanking/app/domain/entities/token"
+	customError "stoneBanking/app/domain/errors"
 	"stoneBanking/app/domain/types"
 	"testing"
 
@@ -20,7 +20,7 @@ func Test_GetBalance(t *testing.T) {
 		input       string
 		runBefore   func(db *sql.DB)
 		want        float64
-		wantErr     bool
+		wantErr     error
 	}{
 		{
 			name: "dado id externo correto, retorna o valor da conta apropriadamente",
@@ -39,25 +39,25 @@ func Test_GetBalance(t *testing.T) {
 			},
 			input:   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeIFI",
 			want:    1,
-			wantErr: false,
+			wantErr: nil,
 		},
 		{
 			name: "dado id incorreto, retorna valor negativo e erro",
 			accountMock: &account.RepositoryMock{
 				GetByIDFunc: func(ctx context.Context, accountID types.ExternalID) (account.Account, error) {
-					return account.Account{}, errors.New("test error")
+					return account.Account{}, customError.ErrorAccountIDNotFound
 				},
 			},
-			input:   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeIFI",
+			input:   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeXXX",
 			want:    -1,
-			wantErr: true,
+			wantErr: customError.ErrorAccountIDNotFound,
 		},
 	}
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			u := New(test.accountMock, test.tokenMock)
 			got, err := u.GetBalance(context.Background(), test.input)
-			assert.Equal(t, (err != nil), test.wantErr)
+			assert.Equal(t, err, test.wantErr)
 			assert.Equal(t, test.want, got)
 		})
 	}
