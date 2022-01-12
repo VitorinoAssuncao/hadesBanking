b2fb7382e6d17ae72059d060fde517c9eb9f1f70
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-12 11:40:30 -0300

    test: add test to validation of creation data

---
+++ app/application/usecase/account/validations/create_account_test.go
@@ -0,0 +1,81 @@
+package account
+
+import (
+	"database/sql"
+	"stoneBanking/app/domain/entities/account"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ValidateAccountData(t *testing.T) {
+	testCases := []struct {
+		name      string
+		input     account.Account
+		runBefore func(db *sql.DB)
+		want      account.Account
+		wantErr   bool
+	}{
+		{
+			name: "dados de conta corretos, deve passar pela validação",
+			input: account.Account{
+				ID:         1,
+				Name:       "Joao do Rio",
+				ExternalID: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeIFI",
+				CPF:        "761.647.810-78",
+				Secret:     "J0@0doR10",
+				Balance:    0,
+			},
+			wantErr: false,
+		},
+		{
+			name: "dados de conta faltando nome, deve apresentar erro",
+			input: account.Account{
+				Name:    "",
+				CPF:     "761.647.810-78",
+				Secret:  "J0@0doR10",
+				Balance: 0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "dados de conta faltando cpf, deve apresentar erro",
+			input: account.Account{
+				Name:    "Joao do Rio",
+				CPF:     "",
+				Secret:  "J0@0doR10",
+				Balance: 0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "dados de conta faltando a senha, deve apresentar erro",
+			input: account.Account{
+				Name:    "Joao do Rio",
+				CPF:     "761.647.810-78",
+				Secret:  "",
+				Balance: 0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "dados de conta apresentam saldo negativo, deve apresentar erro",
+			input: account.Account{
+				ID:         1,
+				Name:       "Joao do Rio",
+				ExternalID: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeIFI",
+				CPF:        "761.647.810-78",
+				Secret:     "J0@0doR10",
+				Balance:    -5,
+			},
+			wantErr: true,
+		},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			err := ValidateAccountData(test.input)
+			assert.Equal(t, (err != nil), test.wantErr)
+		})
+	}
+
+}
