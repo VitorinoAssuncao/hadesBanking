a1aef537069b0077522017b057d4bf2de3e6f9f7
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-12 16:35:45 -0300

    style: fix in typo

--- app/domain/errors/transfers.go
+++ app/domain/errors/transfers.go
@@ -7,6 +7,6 @@ var (
 	ErrorTransferListing                 = errors.New("error when listing all transfers")
 	ErrorTransferCreateOriginError       = errors.New("account origin not found, please validate")
 	ErrorTransferCreateDestinyError      = errors.New("account destination not found, please validate")
-	ErrorTransferCreateInsufficientFunds = errors.New("inssuficient funds")
+	ErrorTransferCreateInsufficientFunds = errors.New("insuficient funds")
 	ErrorTransferCreate                  = errors.New("error when creating transfer, please try again")
 )
