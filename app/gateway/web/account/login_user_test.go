388e9b6b374b8c31c531fa4fb0975fe5fb26bca1
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-14 10:28:15 -0300

    test: add test for login_user

---
+++ app/gateway/web/account/login_user_test.go
@@ -0,0 +1,171 @@
+package account
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	usecase "stoneBanking/app/application/usecase/account"
+	"stoneBanking/app/domain/entities/account"
+	"stoneBanking/app/domain/entities/token"
+	customError "stoneBanking/app/domain/errors"
+	"stoneBanking/app/gateway/web/account/vo/input"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_LoginUser(t *testing.T) {
+	testCases := []struct {
+		name            string
+		accountUsecase  usecase.Usecase
+		tokenRepository token.Repository
+		input           input.CreateAccountVO
+		wantCode        int
+		wantBody        map[string]interface{}
+		wantErr         error
+	}{
+		{
+			name: "with the correct data, log the user and return the authorization token",
+			accountUsecase: usecase.New(
+				&account.RepositoryMock{
+					GetByCPFFunc: func(ctx context.Context, accountCPF string) (account.Account, error) {
+						return account.Account{
+							ID:         1,
+							Name:       "Joao do Rio",
+							ExternalID: "94b9c27e-2880-42e3-8988-62dceb6b6463",
+							CPF:        "761.647.810-78",
+							Secret:     "$2a$14$zmq6uXNuf.1ZwUHHDDAnR.ggIYn0YEnWF/1HeeZrf8d8B55mkk.aq",
+							Balance:    0,
+						}, nil
+					},
+				},
+				&token.RepositoryMock{
+					GenerateTokenFunc: func(accountExternalID string) (signedToken string, err error) {
+						signedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeIFI"
+						return signedToken, nil
+					},
+				}),
+			tokenRepository: &token.RepositoryMock{},
+			input: input.CreateAccountVO{
+				CPF:    "761.647.810-78",
+				Secret: "12344",
+			},
+			wantCode: 200,
+			wantBody: map[string]interface{}{
+				"authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeIFI",
+			},
+			wantErr: nil,
+		},
+		{
+			name: "with the login data withouth cpf, when validating the data return a error",
+			accountUsecase: usecase.New(
+				&account.RepositoryMock{
+					GetByCPFFunc: func(ctx context.Context, accountCPF string) (account.Account, error) {
+						return account.Account{
+							ID:         1,
+							Name:       "Joao do Rio",
+							ExternalID: "94b9c27e-2880-42e3-8988-62dceb6b6463",
+							CPF:        "761.647.810-78",
+							Secret:     "$2a$14$zmq6uXNuf.1ZwUHHDDAnR.ggIYn0YEnWF/1HeeZrf8d8B55mkk.aq",
+							Balance:    0,
+						}, nil
+					},
+				},
+				&token.RepositoryMock{
+					GenerateTokenFunc: func(accountExternalID string) (signedToken string, err error) {
+						signedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeIFI"
+						return signedToken, nil
+					},
+				}),
+			tokenRepository: &token.RepositoryMock{},
+			input: input.CreateAccountVO{
+				CPF:    "",
+				Secret: "12344",
+			},
+			wantCode: 400,
+			wantBody: nil,
+			wantErr:  customError.ErrorAccountLogin,
+		},
+		{
+			name: "with the correct cpf but the wrong password, try to log, but return a error",
+			accountUsecase: usecase.New(
+				&account.RepositoryMock{
+					GetByCPFFunc: func(ctx context.Context, accountCPF string) (account.Account, error) {
+						return account.Account{
+							ID:         1,
+							Name:       "Joao do Rio",
+							ExternalID: "94b9c27e-2880-42e3-8988-62dceb6b6463",
+							CPF:        "761.647.810-78",
+							Secret:     "$2a$14$zmq6uXNuf.1ZwUHHDDAnR.ggIYn0YEnWF/1HeeZrf8d8B55mkk.aq",
+							Balance:    0,
+						}, nil
+					},
+				},
+				&token.RepositoryMock{
+					GenerateTokenFunc: func(accountExternalID string) (signedToken string, err error) {
+						signedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiJjMDM2NDc1Zi1iN2EwLTRmMzQtOGYxZi1jNDM1MTVkMzE3MjQifQ.Vzl8gI6gYbDMTDPhq878f_Wq_b8J0xz81do8XmHeIFI"
+						return signedToken, nil
+					},
+				}),
+			tokenRepository: &token.RepositoryMock{},
+			input: input.CreateAccountVO{
+				CPF:    "761.647.810-78",
+				Secret: "12345",
+			},
+			wantCode: 400,
+			wantBody: nil,
+			wantErr:  customError.ErrorAccountLogin,
+		},
+		{
+			name: "with the correct data, but happens a error when generating the token, and return error",
+			accountUsecase: usecase.New(
+				&account.RepositoryMock{
+					GetByCPFFunc: func(ctx context.Context, accountCPF string) (account.Account, error) {
+						return account.Account{
+							ID:         1,
+							Name:       "Joao do Rio",
+							ExternalID: "94b9c27e-2880-42e3-8988-62dceb6b6463",
+							CPF:        "761.647.810-78",
+							Secret:     "$2a$14$zmq6uXNuf.1ZwUHHDDAnR.ggIYn0YEnWF/1HeeZrf8d8B55mkk.aq",
+							Balance:    0,
+						}, nil
+					},
+				},
+				&token.RepositoryMock{
+					GenerateTokenFunc: func(accountExternalID string) (signedToken string, err error) {
+						return "", customError.ErrorAccountTokenGeneration
+					},
+				}),
+			tokenRepository: &token.RepositoryMock{},
+			input: input.CreateAccountVO{
+				CPF:    "761.647.810-78",
+				Secret: "12344",
+			},
+			wantCode: 500,
+			wantBody: nil,
+			wantErr:  customError.ErrorAccountTokenGeneration,
+		},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			body, _ := json.Marshal(test.input)
+			rec := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodPost, "/account/login", bytes.NewReader(body))
+			controller := New(test.accountUsecase, test.tokenRepository)
+			controller.LoginUser(rec, req)
+
+			assert.Equal(t, test.wantCode, rec.Code)
+
+			if test.wantBody != nil {
+				wantBodyJson, _ := json.Marshal(test.wantBody)
+				assert.JSONEq(t, string(wantBodyJson), rec.Body.String())
+			}
+
+			if test.wantErr != nil {
+				assert.Equal(t, (test.wantErr.Error() + "\n"), rec.Body.String())
+			}
+		})
+	}
+}
