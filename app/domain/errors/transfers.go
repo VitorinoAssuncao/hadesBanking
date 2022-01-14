e145e8eb9e1fb8a865dc15d4fc4c8bb9f2d07d7e
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-14 16:03:51 -0300

    refactor: change in the errors to common errors

--- app/domain/errors/transfers.go
+++ app/domain/errors/transfers.go
@@ -3,10 +3,14 @@ package customError
 import "errors"
 
 var (
-	ErrorTransferAccountNotFound         = errors.New("account not found, please validate the data")
-	ErrorTransferListing                 = errors.New("error when listing all transfers")
-	ErrorTransferCreateOriginError       = errors.New("account origin not found, please validate")
-	ErrorTransferCreateDestinyError      = errors.New("account destination not found, please validate")
-	ErrorTransferCreateInsufficientFunds = errors.New("insuficient funds")
-	ErrorTransferCreate                  = errors.New("error when creating transfer, please try again")
+	ErrorTransferAccountNotFound          = errors.New("account not found, please validate the data")
+	ErrorTransferListing                  = errors.New("error when listing all transfers")
+	ErrorTransferCreateOriginError        = errors.New("account origin not found, please validate")
+	ErrorTransferCreateDestinyError       = errors.New("account destination not found, please validate")
+	ErrorTransferCreateInsufficientFunds  = errors.New("insuficient funds")
+	ErrorTransferCreate                   = errors.New("error when creating transfer, please try again")
+	ErrorTransferOriginEqualDestiny       = errors.New("origin account and destiny account cannot by the same")
+	ErrorTransferAccountOriginIDRequired  = errors.New("field account_origin_id is required")
+	ErrorTransferAccountDestinyIDRequired = errors.New("field account_destiny_id is required")
+	ErrorTransferAmountInvalid            = errors.New("insufficient founds")
 )
--- app/gateway/web/transfer/vo/input/validations/transfer/create_transfer.go
+++ app/gateway/web/transfer/vo/input/validations/transfer/create_transfer.go
@@ -1,24 +1,25 @@
 package transfer
 
 import (
+	customError "stoneBanking/app/domain/errors"
 	"stoneBanking/app/gateway/web/transfer/vo/input"
 )
 
 func ValidateTransferData(transferData input.CreateTransferVO) (input.CreateTransferVO, error) {
 	if transferData.AccountOriginID == transferData.AccountDestinyID {
-		return input.CreateTransferVO{}, ErrorTransferOriginEqualDestiny
+		return input.CreateTransferVO{}, customError.ErrorTransferOriginEqualDestiny
 	}
 
 	if transferData.AccountOriginID == "" {
-		return input.CreateTransferVO{}, ErrorTransferAccountOriginIDRequired
+		return input.CreateTransferVO{}, customError.ErrorTransferAccountOriginIDRequired
 	}
 
 	if transferData.AccountDestinyID == "" {
-		return input.CreateTransferVO{}, ErrorTransferAccountDestinyIDRequired
+		return input.CreateTransferVO{}, customError.ErrorTransferAccountDestinyIDRequired
 	}
 
 	if transferData.Amount <= 0 {
-		return input.CreateTransferVO{}, ErrorTransferAmountInvalid
+		return input.CreateTransferVO{}, customError.ErrorTransferAmountInvalid
 	}
 
 	return transferData, nil
