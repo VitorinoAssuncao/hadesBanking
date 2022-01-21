3c2f5ad4d36884f28bdb30e8240d228c640a3aa3
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-21 17:17:56 -0300

    fix: change in the validation of error

--- app/application/usecase/account/create_account.go
+++ app/application/usecase/account/create_account.go
@@ -2,7 +2,6 @@ package account
 
 import (
 	"context"
-	"errors"
 	validations "stoneBanking/app/application/usecase/account/validations"
 	"stoneBanking/app/domain/entities/account"
 	customError "stoneBanking/app/domain/errors"
@@ -21,10 +20,11 @@ func (usecase *usecase) Create(ctx context.Context, accountData account.Account)
 	_, err = usecase.accountRepository.GetByCPF(ctx, accountData.CPF.ToString())
 	//validate if account with that cpf exist, if not continue the creation of a new account
 	if err == nil {
+		usecase.logRepository.LogError(operation, customError.ErrorAccountCPFExists.Error())
 		return account.Account{}, customError.ErrorAccountCPFExists
 	}
 
-	if !errors.Is(err, customError.ErrorAccountCPFNotFound) {
+	if err == customError.ErrorAccountCPFNotFound {
 		usecase.logRepository.LogError(operation, err.Error())
 		return account.Account{}, customError.ErrorCreateAccount
 	}
