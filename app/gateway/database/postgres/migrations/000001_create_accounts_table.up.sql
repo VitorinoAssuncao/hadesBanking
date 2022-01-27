2a965c9379ede2ca64692e4af250e68d911a79f2
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-27 11:32:28 -0300

    refactor: change in the name of cpf uk

--- app/gateway/database/postgres/migrations/000001_create_accounts_table.up.sql
+++ app/gateway/database/postgres/migrations/000001_create_accounts_table.up.sql
@@ -4,10 +4,12 @@ CREATE TABLE IF NOT EXISTS public.accounts(
     id            INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     external_id   uuid                                         NOT NULL default gen_random_uuid(), 
     name          text                                         NOT NULL,
-    cpf           text                                         NOT NULL UNIQUE,
+    cpf           text                                         NOT NULL,
     secret        text                                         NOT NULL,
     balance       bigint                                       NOT NULL,
     created_at    timestamp with time zone                     NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+    CONSTRAINT account_cpf_uk UNIQUE (cpf)
 );
 
 COMMIT;
