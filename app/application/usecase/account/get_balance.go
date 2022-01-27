47d6d75b9dcad7622aa94e4eeeeb15a238ba5d06
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-27 08:35:10 -0300

    refactor: changed to new return of db func

--- app/application/usecase/account/get_balance.go
+++ app/application/usecase/account/get_balance.go
@@ -9,12 +9,12 @@ import (
 func (usecase *usecase) GetBalance(ctx context.Context, accountID string) (float64, error) {
 	const operation = "Usecase.Account.GetBalance"
 
-	tempAccount, err := usecase.accountRepository.GetByID(ctx, types.ExternalID(accountID))
+	tempResult, err := usecase.accountRepository.GetBalanceByAccountID(ctx, types.ExternalID(accountID))
 	if err != nil {
 		usecase.logger.LogError(operation, err.Error())
 		return -1, customError.ErrorAccountIDNotFound
 	}
 
 	usecase.logger.LogInfo(operation, "balance sucessfully listed")
-	return tempAccount.Balance.ToFloat(), nil
+	return tempResult.ToFloat(), nil
 }
