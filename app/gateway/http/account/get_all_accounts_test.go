0b4809759063f19548620966dbe6d0b89e1c4759
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-28 15:53:01 -0300

    refactor: changed to route test

--- app/gateway/http/account/get_all_accounts_test.go
+++ app/gateway/http/account/get_all_accounts_test.go
@@ -12,38 +12,46 @@ import (
 	customError "stoneBanking/app/domain/errors"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetAll(t *testing.T) {
-	testCases := []struct {
-		name           string
+	const routePattern = "/accounts"
+
+	type fields struct {
 		accountUsecase usecase.Usecase
 		authenticator  token.Authenticator
 		logger         logHelper.Logger
-		wantCode       int
-		wantBody       []map[string]interface{}
+	}
+
+	testCases := []struct {
+		name     string
+		fields   fields
+		wantCode int
+		wantBody []map[string]interface{}
 	}{
 		{
 			name: "with at last one account existing, data from account is returned sucessfully",
-			accountUsecase: usecase.New(
-				&account.RepositoryMock{
-					GetAllFunc: func(ctx context.Context) ([]account.Account, error) {
-						return []account.Account{{
-							ID:         1,
-							Name:       "Joao do Rio",
-							ExternalID: "94b9c27e-2880-42e3-8988-62dceb6b6463",
-							CPF:        "761.647.810-78",
-							Secret:     "J0@0doR10",
-							Balance:    0,
-						}}, nil
+			fields: fields{
+				accountUsecase: usecase.New(
+					&account.RepositoryMock{
+						GetAllFunc: func(ctx context.Context) ([]account.Account, error) {
+							return []account.Account{{
+								ID:         1,
+								Name:       "Joao do Rio",
+								ExternalID: "94b9c27e-2880-42e3-8988-62dceb6b6463",
+								CPF:        "761.647.810-78",
+								Secret:     "J0@0doR10",
+								Balance:    0,
+							}}, nil
+						},
 					},
-				},
-				&token.RepositoryMock{},
-				&logHelper.RepositoryMock{}),
-			authenticator: &token.RepositoryMock{},
-			logger:        &logHelper.RepositoryMock{},
-			wantCode:      http.StatusOK,
+					&token.RepositoryMock{},
+					&logHelper.RepositoryMock{}),
+				authenticator: &token.RepositoryMock{},
+				logger:        &logHelper.RepositoryMock{}},
+			wantCode: http.StatusOK,
 			wantBody: []map[string]interface{}{{
 				"id":         "94b9c27e-2880-42e3-8988-62dceb6b6463",
 				"name":       "Joao do Rio",
@@ -54,17 +62,19 @@ func Test_GetAll(t *testing.T) {
 		},
 		{
 			name: "with one error when listing the accounts, return error for client",
-			accountUsecase: usecase.New(
-				&account.RepositoryMock{
-					GetAllFunc: func(ctx context.Context) ([]account.Account, error) {
-						return []account.Account{{}}, customError.ErrorAccountsListing
+			fields: fields{
+				accountUsecase: usecase.New(
+					&account.RepositoryMock{
+						GetAllFunc: func(ctx context.Context) ([]account.Account, error) {
+							return []account.Account{{}}, customError.ErrorAccountsListing
+						},
 					},
-				},
-				&token.RepositoryMock{},
-				&logHelper.RepositoryMock{}),
-			authenticator: &token.RepositoryMock{},
-			logger:        &logHelper.RepositoryMock{},
-			wantCode:      http.StatusInternalServerError,
+					&token.RepositoryMock{},
+					&logHelper.RepositoryMock{}),
+				authenticator: &token.RepositoryMock{},
+				logger:        &logHelper.RepositoryMock{},
+			},
+			wantCode: http.StatusInternalServerError,
 			wantBody: []map[string]interface{}{{
 				"error": "error when listing all accounts",
 			}},
@@ -72,11 +82,15 @@ func Test_GetAll(t *testing.T) {
 	}
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
+			controller := New(test.fields.accountUsecase, test.fields.authenticator, test.fields.logger)
+			req := httptest.NewRequest(http.MethodGet, "/accounts", nil)
+
+			router := mux.NewRouter()
+			router.HandleFunc(routePattern, controller.GetAll)
 
 			rec := httptest.NewRecorder()
-			req := httptest.NewRequest(http.MethodGet, "/account", nil)
-			controller := New(test.accountUsecase, test.authenticator, test.logger)
-			controller.GetAll(rec, req)
+			router.ServeHTTP(rec, req)
+
 			assert.Equal(t, test.wantCode, rec.Code)
 
 			if test.wantBody != nil {
