fd1adbd60155b3ac645081c7713d6acf70a94f84
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-28 10:58:39 -0300

    feat: add error handling

--- app/gateway/http/middleware/authorization.go
+++ app/gateway/http/middleware/authorization.go
@@ -3,16 +3,22 @@ package middleware
 import (
 	"context"
 	"net/http"
+	customError "stoneBanking/app/domain/errors"
+	"stoneBanking/app/gateway/http/response"
+	"stoneBanking/app/gateway/http/transfer/vo/output"
 )
 
 func (m *Middleware) GetAccountIDFromTokenLogRoutes(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp := response.NewResponse(w)
 		headerToken := r.Header.Get("Authorization")
-		/*		if headerToken == "" {
-					return "", customError.ErrorServerTokenNotFound
-				}
-		*/
-		accountExternalID, _ := m.t.ExtractAccountIDFromToken(headerToken)
+		if headerToken == "" {
+			resp.Unauthorized(output.NewError(customError.ErrorServerTokenNotFound))
+		}
+		accountExternalID, err := m.t.ExtractAccountIDFromToken(headerToken)
+		if err != nil {
+			resp.BadRequest(output.NewError(err))
+		}
 
 		ctx := r.Context()
 		newCtx := context.WithValue(ctx, AccountContextKey, accountExternalID)
