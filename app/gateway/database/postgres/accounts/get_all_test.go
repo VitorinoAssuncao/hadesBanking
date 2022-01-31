3f8b8041b65c835af0713dc7ee32209947be60f3
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-01-31 16:25:33 -0300

    refactor: improved error handling and want

--- app/gateway/database/postgres/accounts/get_all_test.go
+++ app/gateway/database/postgres/accounts/get_all_test.go
@@ -15,8 +15,8 @@ func Test_GetAll(t *testing.T) {
 	testCases := []struct {
 		name    string
 		input   account.Account
-		want    int
-		wantErr bool
+		want    []account.Account
+		wantErr error
 	}{
 		{
 			name: "find and return all the accounts, when at last one exist",
@@ -25,8 +25,14 @@ func Test_GetAll(t *testing.T) {
 				CPF:     "38330499912",
 				Balance: 10000,
 			},
-			want:    1,
-			wantErr: false,
+			want: []account.Account{
+				{
+					Name:    "Joao da Silva",
+					CPF:     "38330499912",
+					Balance: 10000,
+				},
+			},
+			wantErr: nil,
 		},
 	}
 
@@ -40,9 +46,18 @@ func Test_GetAll(t *testing.T) {
 			if err != nil {
 				t.Errorf("error when creating account")
 			}
+
 			got, err := accountRepository.GetAll(ctx)
-			assert.Equal(t, (err != nil), test.wantErr)
-			assert.Equal(t, test.want, len(got))
+			if err == nil {
+				for index, result := range got {
+					test.want[index].ID = result.ID
+					test.want[index].CreatedAt = result.CreatedAt
+					test.want[index].ExternalID = result.ExternalID
+				}
+			}
+
+			assert.Equal(t, test.wantErr, err)
+			assert.Equal(t, test.want, got)
 		})
 	}
 }
