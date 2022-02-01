9f7b0c40c58ca3acd956222446daaeca21d5403d
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-01 16:48:38 -0300

    feat: add debug level to logger

--- app/domain/entities/logger/mock.go
+++ app/domain/entities/logger/mock.go
@@ -6,10 +6,14 @@ type RepositoryMock struct {
 	LogInfoFunc                 func(operation string, msg string)
 	LogWarnFunc                 func(operation string, msg string)
 	LogErrorFunc                func(operation string, msg string)
+	LogDebugFunc                func(operation string, msg string)
 	SetRequestIDFromContextFunc func(ctx context.Context) Logger
 }
 
 func (r *RepositoryMock) LogInfo(operation string, msg string) {
+}
+
+func (r *RepositoryMock) LogDebug(operation string, msg string) {
 }
 
 func (r *RepositoryMock) LogWarn(operation string, msg string) {
