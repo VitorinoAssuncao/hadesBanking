8c9f2f85df35504cac10a77725e4a2bb5685cb2f
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-01 11:57:33 -0300

    feat: add function to set the requestID key

--- app/domain/entities/logger/mock.go
+++ app/domain/entities/logger/mock.go
@@ -1,9 +1,12 @@
 package logHelper
 
+import "context"
+
 type RepositoryMock struct {
-	LogInfoFunc  func(operation string, msg string)
-	LogWarnFunc  func(operation string, msg string)
-	LogErrorFunc func(operation string, msg string)
+	LogInfoFunc                 func(operation string, msg string)
+	LogWarnFunc                 func(operation string, msg string)
+	LogErrorFunc                func(operation string, msg string)
+	SetRequestIDFromContextFunc func(ctx context.Context) Logger
 }
 
 func (r *RepositoryMock) LogInfo(operation string, msg string) {
@@ -13,4 +16,8 @@ func (r *RepositoryMock) LogWarn(operation string, msg string) {
 }
 
 func (r *RepositoryMock) LogError(operation string, msg string) {
+}
+
+func (r *RepositoryMock) SetRequestIDFromContext(ctx context.Context) Logger {
+	return r
 }
