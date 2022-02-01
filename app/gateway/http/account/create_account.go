5922b8eefd365997cd82cb77fa1432a67e5fe341
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-01 08:46:04 -0300

    style: simplify receivers

--- app/gateway/http/account/create_account.go
+++ app/gateway/http/account/create_account.go
@@ -20,41 +20,41 @@ import (
 //@Failure	400 {object} response.OutputError
 //@Failure 500 {object} response.OutputError
 //@Router /accounts [POST]
-func (controller *Controller) Create(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) Create(w http.ResponseWriter, r *http.Request) {
 	const operation = "Gateway.Rest.Account.Create"
 	resp := response.NewResponse(w)
 
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		controller.log.LogError(operation, err.Error())
+		c.log.LogError(operation, err.Error())
 		resp.BadRequest(response.NewError(err))
 		return
 	}
 	defer r.Body.Close()
 
-	controller.log.LogInfo(operation, "unmarshal the data to a internal object")
+	c.log.LogInfo(operation, "unmarshal the data to a internal object")
 	var accountInput = input.CreateAccountVO{}
 	err = json.Unmarshal(reqBody, &accountInput)
 	if err != nil {
-		controller.log.LogError(operation, err.Error())
+		c.log.LogError(operation, err.Error())
 		resp.BadRequest(response.NewError(err))
 		return
 	}
 
 	accountInput.CPF = accountInput.CPF.TrimCPF()
 
-	controller.log.LogInfo(operation, "begin the validation of the input data")
+	c.log.LogInfo(operation, "begin the validation of the input data")
 	accountInput, err = validations.ValidateAccountInput(accountInput)
 	if err != nil {
-		controller.log.LogError(operation, err.Error())
+		c.log.LogError(operation, err.Error())
 		resp.BadRequest(response.NewError(err))
 		return
 	}
 
 	accountData := accountInput.ToEntity()
-	account, err := controller.usecase.Create(r.Context(), accountData)
+	account, err := c.usecase.Create(r.Context(), accountData)
 	if err != nil {
-		controller.log.LogError(operation, err.Error())
+		c.log.LogError(operation, err.Error())
 		resp.InternalError(response.NewError(err))
 		return
 	}
--- app/gateway/http/account/get_all_accounts.go
+++ app/gateway/http/account/get_all_accounts.go
@@ -14,18 +14,18 @@ import (
 //@Failure	400 {object} response.OutputError
 //@Failure 500 {object} response.OutputError
 //@Router /accounts [GET]
-func (controller *Controller) GetAll(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) GetAll(w http.ResponseWriter, r *http.Request) {
 	const operation = "Gateway.Rest.Account.GetAll"
 	resp := response.NewResponse(w)
 
-	accounts, err := controller.usecase.GetAll(r.Context())
+	accounts, err := c.usecase.GetAll(r.Context())
 	if err != nil {
-		controller.log.LogError(operation, err.Error())
+		c.log.LogError(operation, err.Error())
 		resp.InternalError(response.NewError(err))
 		return
 	}
 
 	accountsOutput := output.ToAccountsOutput(accounts)
-	controller.log.LogInfo(operation, "accounts created successfully")
+	c.log.LogInfo(operation, "accounts created successfully")
 	resp.Ok(accountsOutput)
 }
