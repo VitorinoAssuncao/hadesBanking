70aea82eca72e09cf4f944eece7523258dc87d72
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-03 17:50:13 -0300

    refactor: changed function setRequestID

--- app/domain/entities/logger/mock.go
+++ app/domain/entities/logger/mock.go
@@ -7,7 +7,7 @@ type RepositoryMock struct {
 	LogWarnFunc                 func(operation string, msg string)
 	LogErrorFunc                func(operation string, msg string)
 	LogDebugFunc                func(operation string, msg string)
-	SetRequestIDFromContextFunc func(ctx context.Context) Logger
+	SetRequestIDFromContextFunc func(ctx context.Context)
 }
 
 func (r *RepositoryMock) LogInfo(operation string, msg string) {
@@ -22,6 +22,5 @@ func (r *RepositoryMock) LogWarn(operation string, msg string) {
 func (r *RepositoryMock) LogError(operation string, msg string) {
 }
 
-func (r *RepositoryMock) SetRequestIDFromContext(ctx context.Context) Logger {
-	return r
+func (r *RepositoryMock) SetRequestIDFromContext(ctx context.Context) {
 }
