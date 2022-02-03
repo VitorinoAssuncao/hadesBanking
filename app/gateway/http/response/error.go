36c15b28c3b0ba8aaed47b57613bbcef76d40154
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-03 13:21:56 -0300

    feat: add function to generate output

--- app/gateway/http/response/error.go
+++ app/gateway/http/response/error.go
@@ -9,3 +9,11 @@ func NewError(err error) *OutputError {
 		Error: err.Error(),
 	}
 }
+
+func NewErrors(errs []error) *[]OutputError {
+	var output = make([]OutputError, 0)
+	for _, err := range errs {
+		output = append(output, OutputError{Error: err.Error()})
+	}
+	return &output
+}
