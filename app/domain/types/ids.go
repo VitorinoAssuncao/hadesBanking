2891232f3b74a6544c11e5c39a56ebb4f8dcbf1a
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-03 11:13:28 -0300

    feat: add function to convert to uuid

--- app/domain/types/ids.go
+++ app/domain/types/ids.go
@@ -1,7 +1,14 @@
 package types
 
+import "github.com/google/uuid"
+
 type ExternalID string
 
 type InternalID int
 
 type ContextKey string
+
+func (e ExternalID) ToUUID() uuid.UUID {
+	parsed, _ := uuid.Parse(string(e))
+	return parsed
+}
