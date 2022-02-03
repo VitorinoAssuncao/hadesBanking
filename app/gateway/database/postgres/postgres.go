a6bbe5e439e0386ba191dc64fbab517f7eea86b0
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-03 09:42:57 -0300

    refactor: changed connector

--- app/gateway/database/postgres/postgres.go
+++ app/gateway/database/postgres/postgres.go
@@ -1,24 +1,25 @@
 package postgres
 
 import (
-	"database/sql"
+	"context"
 	"errors"
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v4"
 
 	"stoneBanking/app/common/utils/config"
 )
 
-func InitializeDatabase(config config.Config) (*sql.DB, error) {
+func InitializeDatabase(config config.Config) (*pgx.Conn, error) {
 	const migrationPath = "file://app/gateway/database/postgres/migrations"
 
 	dbUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
 		config.DBUser, config.DBPass, config.DBHost, config.DBPort, config.DBBase, config.DBSSLMode)
 
-	db, _ := sql.Open("postgres", dbUrl)
+	db, _ := pgx.Connect(context.Background(), dbUrl)
 	err := Migrate(migrationPath, dbUrl)
 	if err != nil {
 		if !errors.Is(err, migrate.ErrNoChange) {
