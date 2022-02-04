c17987f8df69a67c57ea8ba1598128c5f87321c1
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-04 12:13:56 -0300

    refactor: take out unused variable

--- app/gateway/database/postgres/transfers/main_test.go
+++ app/gateway/database/postgres/transfers/main_test.go
@@ -4,11 +4,7 @@ import (
 	"os"
 	"stoneBanking/app/gateway/database/postgres/pgtest"
 	"testing"
-
-	"github.com/jackc/pgx/v4/pgxpool"
 )
-
-var testPool *pgxpool.Pool
 
 func TestMain(m *testing.M) {
 	os.Exit(pgtest.SetupTests(m))
