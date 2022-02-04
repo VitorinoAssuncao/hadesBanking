a2b7e1c5d7997a39f85996092f55764e76c86b3d
Vitorino Assuncao <vitorino.muller@gmail.com>
2022-02-04 11:57:47 -0300

    refactor: separate the functions of main test

---
+++ app/gateway/database/postgres/pgtest/docker.go
@@ -0,0 +1,69 @@
+package pgtest
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/ory/dockertest"
+)
+
+var testPool *pgxpool.Pool
+
+func SetupTests(m *testing.M) int {
+	dbName := GetRandomDBName()
+	pool, err := dockertest.NewPool("")
+	ctx := context.Background()
+	if err != nil {
+		log.Fatalf("error when trying to connect to docker")
+	}
+
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "13",
+		Env: []string{
+			"POSTGRES_PASSWORD=secret",
+			"POSTGRES_USER=user_name",
+			"POSTGRES_DB=" + dbName,
+			"listen_addresses = '*'",
+		},
+	})
+	if err != nil {
+		log.Fatalf("has not possible to connect to resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	dbUrl := fmt.Sprintf("postgres://user_name:secret@%s/%s?sslmode=disable", hostAndPort, dbName)
+	resource.Expire(120) //nolint: errorlint
+	pool.MaxWait = 120 * time.Second
+
+	if err = pool.Retry(func() error {
+		testPool, err := pgx.Connect(ctx, dbUrl)
+		if err != nil {
+			return err
+		}
+		return testPool.Ping(ctx)
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	newConn, err := pgxpool.Connect(ctx, dbUrl)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	testPool = newConn
+	defer teardown()
+
+	return m.Run()
+}
+
+func GetConn() *pgxpool.Pool {
+	return testPool
+}
+
+func teardown() {
+	testPool.Close()
+}
--- app/gateway/database/postgres/pgtest/postgres.go
+++ app/gateway/database/postgres/pgtest/postgres.go
@@ -1,13 +1,45 @@
 package pgtest
 
 import (
+	"context"
 	"crypto/rand"
 	"fmt"
+	"log"
 	"math"
 	"math/big"
+	"stoneBanking/app/gateway/database/postgres"
+	"strings"
+	"testing"
+
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 func GetRandomDBName() string {
 	n, _ := rand.Int(rand.Reader, big.NewInt(math.MaxInt32))
 	return fmt.Sprintf("db_%d", n)
+}
+
+func SetDatabase(t *testing.T, dbName string) *pgxpool.Pool {
+	err := createDB(dbName, testPool)
+	if err != nil {
+		log.Fatalf("has not possible to create the new database error:%s", err.Error())
+	}
+	conn := testPool
+	dbUrl := strings.Replace(conn.Config().ConnString(), conn.Config().ConnConfig.Database, dbName, 1)
+	pgxConn, err := pgxpool.Connect(context.Background(), dbUrl)
+	if err != nil {
+		log.Fatalf("has not possible to connect to database %v", err.Error())
+	}
+	migrationPath := "file:../migrations"
+
+	err = postgres.Migrate(migrationPath, dbUrl)
+	if err != nil {
+		log.Fatalf("error during migration %v", err)
+	}
+	return pgxConn
+}
+
+func createDB(dbName string, conn *pgxpool.Pool) error {
+	_, err := conn.Exec(context.Background(), fmt.Sprintf(`CREATE DATABASE %s`, dbName))
+	return err
 }
